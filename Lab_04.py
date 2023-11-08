a5111763699a1e14272d807bef9282107d55e3d0
Jon Defilla <jdefilla@gmail.com>
2023-11-08 18:45:55 +0100

    lab 04 add working reinforce algo

--- Lab_04.py
+++ Lab_04.py
@@ -5,9 +5,7 @@ from scipy.special import softmax
 
 
 class CartPoleREINFORCEAgent:
-    """
-    REINFORCE Agent for the CartPole environment
-    """
+
     def __init__(self, num_episodes=500, learning_rate=0.01, gamma=0.99):
         self.num_episodes = num_episodes
         self.learning_rate = learning_rate
@@ -39,7 +37,6 @@ class CartPoleREINFORCEAgent:
             while not done:
                 action = self.choose_action(state)
                 next_state, reward, done, _, _ = self.env.step(action)
-                next_state = np.array(next_state)  # Ensure next_state is a NumPy array
                 transitions.append((state, action, reward))
                 state = next_state
 
@@ -55,16 +52,20 @@ class CartPoleREINFORCEAgent:
     def learn(self, transitions):
         for i, (state, action, reward) in enumerate(transitions):
             # Calculate the discounted return
+            # G_t = R_(t+1) + gamma * R_(t+2) + gamma^2 * R_(t+3) + ... + gamma^(T-t-1) * R_T
+            # R_(t+1) is the reward received after taking an action at time step t.
+            # gamma is the discount factor, which is a number between 0 and 1, which determines the present value of future rewards
+            # T is the final time step of the episode.
             G = sum(self.gamma ** t * r for t, (_, _, r) in enumerate(transitions[i:]))
 
             # Calculate the policy gradient
-            probs = self.policy(state)
-            dsoftmax = probs * (1 - probs[action])
-            dlog = -dsoftmax / probs[action]
-            grad = np.outer(dlog, state).T
+            probs = self.policy(state)  # probability of taking action a in state s with current policy
+            dsoftmax = softmax(state.dot(self.theta)) * (1 - softmax(state.dot(self.theta)))
+            dlog = dsoftmax / probs  # derivative of log pi(a|s, theta)
+            grad = state.reshape(-1, 1) * dlog  # gradient of log pi(a|s, theta) * G
 
             # Update policy parameters
-            self.theta += self.learning_rate * grad * G
+            self.theta[:, action] += self.learning_rate * grad[:, action] * G
 
     def plot_learning(self, episode_rewards):
         plt.plot(episode_rewards)
@@ -74,7 +75,11 @@ class CartPoleREINFORCEAgent:
 
 
 def load_reinforce():
-    agent = CartPoleREINFORCEAgent()
+    num_episodes = 500
+    learning_rate = 0.01
+    gamma = 0.99
+
+    agent = CartPoleREINFORCEAgent(num_episodes, learning_rate, gamma)
     episode_rewards = agent.train()
     agent.plot_learning(episode_rewards)
     return agent
