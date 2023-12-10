1ee1f7bea710bfeb9d142ab9fab3f7b193113289
Jon Defilla <jdefilla@gmail.com>
2023-12-10 20:45:14 +0100

    DQN kind of working

--- Lab_13_DQN.py
+++ Lab_13_DQN.py
@@ -1,36 +1,31 @@
 # import the necessary libraries
-import numpy as np
 import random
+from collections import deque
+
+import numpy as np
+from tensorflow import gather_nd
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.losses import mean_squared_error
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import RMSprop
-from collections import deque
-from tensorflow import gather_nd
-from tensorflow.keras.losses import mean_squared_error
 
 
-class DeepQLearning:
+class DQN:
 
-    ###########################################################################
-    #   START - __init__ function
-    ###########################################################################
-    # INPUTS:
-    # env - Cart Pole environment
-    # gamma - discount rate
-    # epsilon - parameter for epsilon-greedy approach
-    # numberEpisodes - total number of simulation episodes
-
-    def __init__(self, env, gamma, epsilon, numberEpisodes):
-
+    def __init__(self, env, gamma, epsilon):
+        """
+        :param env: stable baseline env
+        :param gamma: discount factor
+        :param epsilon: for epsilon-greedy policy
+        """
         self.env = env
         self.gamma = gamma
         self.epsilon = epsilon
-        self.numberEpisodes = numberEpisodes
 
         # state dimension
-        self.stateDimension = 4
+        self.stateDimension = 8
         # action dimension
-        self.actionDimension = 2
+        self.actionDimension = 4
         # this is the maximum size of the replay buffer
         self.replayBufferSize = 300
         # this is the size of the training batch that is randomly sampled from the replay buffer
@@ -53,69 +48,50 @@ class DeepQLearning:
 
         # this is the main network
         # create network
-        self.mainNetwork = self.createNetwork()
+        self.onlineNetwork = self.createNetwork()
 
         # this is the target network
         # create network
         self.targetNetwork = self.createNetwork()
 
         # copy the initial weights to targetNetwork
-        self.targetNetwork.set_weights(self.mainNetwork.get_weights())
+        self.targetNetwork.set_weights(self.onlineNetwork.get_weights())
 
         # this list is used in the cost function to select certain entries of the
         # predicted and true sample matrices in order to form the loss
         self.actionsAppend = []
 
-    ###########################################################################
-    #   END - __init__ function
-    ###########################################################################
-
-    ###########################################################################
-    # START - function for defining the loss (cost) function
-    # INPUTS:
-    #
-    # y_true - matrix of dimension (self.batchReplayBufferSize,2) - this is the target
-    # y_pred - matrix of dimension (self.batchReplayBufferSize,2) - this is predicted by the network
-    #
-    # - this function will select certain row entries from y_true and y_pred to form the output
-    # the selection is performed on the basis of the action indices in the list  self.actionsAppend
-    # - this function is used in createNetwork(self) to create the network
-    #
-    # OUTPUT:
-    #
-    # - loss - watch out here, this is a vector of (self.batchReplayBufferSize,1),
-    # with each entry being the squared error between the entries of y_true and y_pred
-    # later on, the tensor flow will compute the scalar out of this vector (mean squared error)
-    ###########################################################################
-
     def my_loss_fn(self, y_true, y_pred):
+        """
+        This function will select certain row entries from y_true and y_pred to form the output
+        the selection is performed on the basis of the action indices in the list  self.actionsAppend
+        this function is used in createNetwork(self) to create the network
 
+        :param y_true: matrix of dimension (self.batchReplayBufferSize,2) - this is the target
+        :param y_pred: matrix of dimension (self.batchReplayBufferSize,2) - this is predicted by the network
+        :return: - loss - watch out here, this is a vector of (self.batchReplayBufferSize,1),
+                   with each entry being the squared error between the entries of y_true and y_pred
+                   later on, the tensor flow will compute the scalar out of this vector (mean squared error)
+        """
         s1, s2 = y_true.shape
-        # print(s1,s2)
+        print(f"y_true shape: {y_true.shape}")
+        print(f"y_pred shape: {y_pred.shape}")
 
         # this matrix defines indices of a set of entries that we want to
         # extract from y_true and y_pred
         # s2=2
         # s1=self.batchReplayBufferSize
-        indices = np.zeros(shape=(s1, s2))
+        indices = np.zeros(shape=(s1, 2))
         indices[:, 0] = np.arange(s1)
         indices[:, 1] = self.actionsAppend
 
-        # gather_nd and mean_squared_error are TensorFlow functions
-        loss = mean_squared_error(gather_nd(y_true, indices=indices.astype(int)), gather_nd(y_pred, indices=indices.astype(int)))
-        # print(loss)
+        y_true_gathered = gather_nd(y_true, indices=indices.astype(int))
+        y_pred_gathered = gather_nd(y_pred, indices=indices.astype(int))
+
+        loss = mean_squared_error(y_true_gathered, y_pred_gathered)
+        print(loss)
         return loss
-        ###########################################################################
 
-    #   END - of function my_loss_fn
-    ###########################################################################
-
-    ###########################################################################
-    #   START - function createNetwork()
-    # this function creates the network
-    ###########################################################################
-
-    # create a neural network
     def createNetwork(self):
         model = Sequential()
         model.add(Dense(128, input_dim=self.stateDimension, activation='relu'))
@@ -125,20 +101,10 @@ class DeepQLearning:
         model.compile(optimizer=RMSprop(), loss=self.my_loss_fn, metrics=['accuracy'])
         return model
 
-    ###########################################################################
-    #   END - function createNetwork()
-    ###########################################################################
-
-    ###########################################################################
-    #   START - function trainingEpisodes()
-    #   - this function simulates the episodes and calls the training function
-    #   - trainNetwork()
-    ###########################################################################
-
-    def trainingEpisodes(self):
+    def learn(self, total_timesteps):
 
         # here we loop through the episodes
-        for indexEpisode in range(self.numberEpisodes):
+        for indexEpisode in range(total_timesteps):
 
             # list that stores rewards per episode - this is necessary for keeping track of convergence
             rewardsEpisode = []
@@ -146,17 +112,19 @@ class DeepQLearning:
             print("Simulating episode {}".format(indexEpisode))
 
             # reset the environment at the beginning of every episode
-            (currentState, _) = self.env.reset()
+            currentState = self.env.reset()[0]  # fixme: for vectorized envs > 1 returns S of all envs
 
             # here we step from one state to another
             # this will loop until a terminal state is reached
             terminalState = False
             while not terminalState:
+
                 # select an action on the basis of the current state, denoted by currentState
-                action = self.selectAction(currentState, indexEpisode)
+                # fixme: here action should be an array for vectorized envs
+                action = self.predict(currentState, indexEpisode)
 
                 # here we step and return the state, reward, and boolean denoting if the state is a terminal state
-                (nextState, reward, terminalState, _, _) = self.env.step(action)
+                (nextState, reward, terminalState, _) = self.env.step([action])
                 rewardsEpisode.append(reward)
 
                 # add current state, action, reward, next state, and terminal flag to the replay buffer
@@ -171,23 +139,17 @@ class DeepQLearning:
             print("Sum of rewards {}".format(np.sum(rewardsEpisode)))
             self.sumRewardsEpisode.append(np.sum(rewardsEpisode))
 
-    ###########################################################################
-    #   END - function trainingEpisodes()
-    ###########################################################################
-
-    ###########################################################################
-    #    START - function for selecting an action: epsilon-greedy approach
-    ###########################################################################
-    # this function selects an action on the basis of the current state
-    # INPUTS:
-    # state - state for which to compute the action
-    # index - index of the current episode
-    def selectAction(self, state, index):
-        import numpy as np
+    def predict(self, state, episode_index):
+        """
+        This function selects an action on the basis of the current state
+        :param state: state for which to compute the action
+        :param episode_index:  index of the current episode
+        :return:
+        """
 
         # first index episodes we select completely random actions to have enough exploration
         # change this
-        if index < 1:
+        if episode_index < 1:
             return np.random.choice(self.actionDimension)
 
             # Returns a random real number in the half-open interval [0.0, 1.0)
@@ -195,7 +157,7 @@ class DeepQLearning:
         randomNumber = np.random.random()
 
         # after index episodes, we slowly start to decrease the epsilon parameter
-        if index > 200:
+        if episode_index > 200:
             self.epsilon = 0.999 * self.epsilon
 
         # if this condition is satisfied, we are exploring, that is, we select random actions
@@ -208,7 +170,7 @@ class DeepQLearning:
             # we return the index where Qvalues[state,:] has the max value
             # that is, since the index denotes an action, we select greedy actions
 
-            Qvalues = self.mainNetwork.predict(state.reshape(1, 4))
+            Qvalues = self.onlineNetwork.predict(state.reshape(1, self.stateDimension))
 
             return np.random.choice(np.where(Qvalues[0, :] == np.max(Qvalues[0, :]))[0])
             # here we need to return the minimum index since it can happen
@@ -220,16 +182,7 @@ class DeepQLearning:
             # that is why we need to have np.random.choice(np.where(a==np.max(a))[0])
             # note that zero has to be added here since np.where() returns a tuple
 
-    ###########################################################################
-    #    END - function selecting an action: epsilon-greedy approach
-    ###########################################################################
-
-    ###########################################################################
-    #    START - function trainNetwork() - this function trains the network
-    ###########################################################################
-
     def trainNetwork(self):
-
         # if the replay buffer has at least batchReplayBufferSize elements,
         # then train the model
         # otherwise wait until the size of the elements exceeds batchReplayBufferSize
@@ -241,8 +194,8 @@ class DeepQLearning:
             # here we form current state batch
             # and next state batch
             # they are used as inputs for prediction
-            currentStateBatch = np.zeros(shape=(self.batchReplayBufferSize, 4))
-            nextStateBatch = np.zeros(shape=(self.batchReplayBufferSize, 4))
+            currentStateBatch = np.zeros(shape=(self.batchReplayBufferSize, self.stateDimension))
+            nextStateBatch = np.zeros(shape=(self.batchReplayBufferSize, self.stateDimension))
             # this will enumerate the tuple entries of the randomSampleBatch
             # index will loop through the number of tuples
             for index, tupleS in enumerate(randomSampleBatch):
@@ -254,13 +207,13 @@ class DeepQLearning:
             # here, use the target network to predict Q-values
             QnextStateTargetNetwork = self.targetNetwork.predict(nextStateBatch)
             # here, use the main network to predict Q-values
-            QcurrentStateMainNetwork = self.mainNetwork.predict(currentStateBatch)
+            QcurrentStateMainNetwork = self.onlineNetwork.predict(currentStateBatch)
 
             # now, we form batches for training
             # input for training
             inputNetwork = currentStateBatch
             # output for training
-            outputNetwork = np.zeros(shape=(self.batchReplayBufferSize, 2))
+            outputNetwork = np.zeros(shape=(self.batchReplayBufferSize, self.actionDimension))
 
             # this list will contain the actions that are selected from the batch
             # this list is used in my_loss_fn to define the loss-function
@@ -283,7 +236,7 @@ class DeepQLearning:
                 outputNetwork[index, action] = y
 
             # here, we train the network
-            self.mainNetwork.fit(inputNetwork, outputNetwork, batch_size=self.batchReplayBufferSize, verbose=0, epochs=100)
+            self.onlineNetwork.fit(inputNetwork, outputNetwork, batch_size=self.batchReplayBufferSize, verbose=0, epochs=100)
 
             # after updateTargetNetworkPeriod training sessions, update the coefficients
             # of the target network
@@ -291,11 +244,8 @@ class DeepQLearning:
             self.counterUpdateTargetNetwork += 1
             if (self.counterUpdateTargetNetwork > (self.updateTargetNetworkPeriod - 1)):
                 # copy the weights to targetNetwork
-                self.targetNetwork.set_weights(self.mainNetwork.get_weights())
+                self.targetNetwork.set_weights(self.onlineNetwork.get_weights())
                 print("Target network updated!")
                 print("Counter value {}".format(self.counterUpdateTargetNetwork))
                 # reset the counter
                 self.counterUpdateTargetNetwork = 0
-    ###########################################################################
-    #    END - function trainNetwork()
-    ###########################################################################
